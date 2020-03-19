29a828a17c09c41e7976cd4b61dae9e908cf12a2
apple <apple@MacBook.attlocal.net>
2020-03-19 08:07:06 -0500

    working on models...

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    breed = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    length = models.IntegerField()
+    region = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+class Finch: 
+    def __init__(self, breed, color, length, region):
+        self.breed = breed
+        self.color = color
+        self.length = length
+        self.region = region
+
+finches = [
+    Finch('House Finch', 'Rose', 5.5, 'North America'),
+    Finch('European Goldfinch', 'Gold and red', 6.0, 'Europe'),
+
+]
+
 def home(request):
     return HttpResponse('<h1>Hello Finch Collectors!</h1>')
 
 def about(request):
     return render(request, 'about.html' )
+
+def finches_index(request):
+    return render(request, 'finches/index.html', { 'finches': finches })
