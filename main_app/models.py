5b1516b11af6ff9e6390f8b8336089dbc43cd1c8
apple <apple@MacBook.attlocal.net>
2020-03-22 12:13:49 -0500

    Listening 1:M...

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEDIUM = (
+    ('D', 'Digital'),
+    ('C', 'Compact Disc'),
+    ('V', 'Vinyl'),
+)
+
 # Create your models here.
 class Record(models.Model):
     artist = models.CharField(max_length=100)
@@ -12,3 +18,16 @@ class Record(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'record_id': self.id})
+
+class Listening(models.Model):
+    date = models.DateField()
+    medium = models.CharField(
+        max_length=1,
+        choices=MEDIUM,
+        default=MEDIUM[0][0]
+        )
+
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_medium_display()} on {self.date}"
