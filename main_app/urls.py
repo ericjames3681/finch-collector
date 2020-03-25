f35240ac4fa23ef4f7bad61ddc609cfaaed65a7c
apple <apple@MacBook.attlocal.net>
2020-03-25 07:19:18 -0500

    Wednesday AM touchups...

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path('records/<int:pk>/delete/', views.RecordDelete.as_view(), name='records_delete'),
     path('records/<int:record_id>/add_listening/', views.add_listening, name='add_listening'),
     path('records/<int:record_id>/assoc_dist/<int:distributor_id>/', views.assoc_dist, name='assoc_dist'),
-    path('records/<int:record_id>/unassoc_dist/<int:distributor_id>/', views.unassoc_dist, name='unassoc_dist')
+    path('records/<int:record_id>/unassoc_dist/<int:distributor_id>/', views.unassoc_dist, name='unassoc_dist'),
+    path('distributors/', views.DistributorList.as_view(), name='distributors_index'),
+    path('distributors/<int:pk>/', views.DistributorDetail.as_view(), name='distributors_detail'),
+    path('distributors/create/', views.DistributorCreate.as_view(), name='distributors_create'),
+    path('distributors/<int:pk>/update/', views.DistributorUpdate.as_view(), name='distributors_update'),
+    path('distributors/<int:pk>/delete/', views.DistributorDelete.as_view(), name='distributors_delete'),
 ]
